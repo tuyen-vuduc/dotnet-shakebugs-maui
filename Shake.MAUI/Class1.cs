be06d88f0b6306d55451b74cba69b05c4334b75b
Tuyen Vu <tuyen.vuduc0902@gmail.com>
2024-05-22 06:17:55 +0700

    - Empty lib

---
+++ Shake.MAUI/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Shake.MAUI;
+
+// All the code in this file is included in all platforms.
+public class Class1
+{
+}
+
---
+++ Shake.MAUI/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Shake.MAUI
+{
+	// All the code in this file is only included on Tizen.
+	public class PlatformClass1
+	{
+	}
+}
