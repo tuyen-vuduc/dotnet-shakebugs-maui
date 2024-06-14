55782e95f5f260f1c5bd2c6f2958fc3af5d036a9
Tuyen Vu <tuyenvd@newwave.com.vn>
2024-06-14 13:33:26 +0700

    - attempt to trigger gradle process

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 dotnet nuget locals -c all
+
+# Build to trigger gradle process
+dotnet build -t:Clean,Rebuild src/ShakeBugsMauiQs/ShakeBugsMauiQs.csproj
+
 dotnet pack -c Release -t:Clean,Rebuild src/ShakeBugs.MAUI/ShakeBugs.MAUI.csproj --output $PWD/nugets -p:AndroidSdkDirectory=$ANDROID_HOME
